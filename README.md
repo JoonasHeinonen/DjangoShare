b3fdde0d2ba41dc74ecd610b9f901b7546e574fa
Joonas Heinonen <joonas.heinonen@edu.turkuamk.fi>
2020-02-12 19:55:50 +0200

    Added models etc.

--- README.md
+++ README.md
@@ -12,14 +12,41 @@ git clone https://github.com/JoonasHeinonen/DjangoShare
 cd DjangoShare
 ```
 
-Installing virtualenvironment will require these steps.
+Installing virtualenvironment will require these steps:
 
 ```venv
 virtualenv venv
 ```
 
-Then, activate the virtual environment.
+Then, activate the virtual environment:
 
 ```
 venv\Scripts\activate
 ```
+
+Next, move to the folder itself:
+
+```
+cd djangoshare
+```
+
+Install requirements from requirements.txt-file:
+
+```
+pip install -r requirements.txt
+```
+
+Run database migrations:
+
+```
+python manage.py makemigrations
+python manage.py migrate
+```
+
+And now, run the software itself.
+
+```
+python manage.py runserver
+```
+
+Congrats! You're ready to run the software!
--- djangoshare/posts/models.py
+++ djangoshare/posts/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    category_name = models.CharField(max_length=100)
+    parent = models.ForeignKey('self', blank=True, null=True, related_name='children', on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name_plural = "categories"
+
+    def __str__(self):
+        full_path = [self.category_name]
+        k = self.parent
+        while k is not None:
+            full_path.append(k.category_name)
+            k = k.parent
+        return ' -> '.join(full_path[::-1])
+
+class Post(models.Model):
+    category      = models.ForeignKey(Category, related_name="posts", on_delete=models.CASCADE)
+    title         = models.CharField(max_length=100)
+    description   = models.CharField(max_length=1000)
+    pub_date      = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.title
+
+class Comment(models.Model):
+    post          = models.ForeignKey(Post, related_name="comments", on_delete=models.CASCADE)
+    title         = models.CharField(max_length=60)
+    content       = models.CharField(max_length=500)
+    pub_date      = models.DateTimeField('date published')
