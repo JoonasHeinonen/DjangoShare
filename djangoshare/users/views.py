9c44b1748aff1e89be464071c79874ff0e2960dc
Joonas <joonas.heinonen@edu.turkuamk.fi>
2020-02-15 14:46:05 +0200

    Implemented login and logout.

--- djangoshare/users/views.py
+++ djangoshare/users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from .forms import UserRegisterForm
+from django.contrib.auth.decorators import login_required
 
 # def login(request):
 #     return render(request, 'users/login.html')
@@ -11,8 +12,12 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}')
-            return redirect('index')
+            messages.success(request, f'An account with username {username} has been created! You can now log in with this account!')
+            return redirect('login')
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
+
+@login_required
+def profile(request):
+    return render(request, 'users/profile.html')
